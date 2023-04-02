980ad282ee7b3e5ee355f13f43e01cd38e69691f
bliztle <asbjoern.r.e@gmail.com>
2023-04-03 01:50:47 +0200

    First commit. Limited debugging support, no tests

---
+++ src/example-import.ts
@@ -0,0 +1,1 @@
+export const exampleImport = 'example-import';
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { exampleImport } from "./example-import";
+
+console.log("Hello World!");
+
+const i = 2;
+
+console.log("Imported from file: ", exampleImport, "\n", i);
