374bdb6eef908137f852f3b719fa42cafa15bb8a
Markus Dam <markus.dam123@gmail.com>
2023-04-23 02:29:29 +0200

    core code

--- src/example-import.test.ts
+++
@@ -1,5 +0,0 @@
-import { exampleImport } from "./example-import";
-
-test("Example Import", () => {
-    expect(exampleImport).toBe("example-import");
-});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,48 @@
-import { exampleImport } from "./example-import";
+/// <reference lib="webworker" />
 
-console.log("Hello World!");
+// Declares type of self,
+declare const self: DedicatedWorkerGlobalScope & {
+    getData: () => Promise<unknown | never>;
+    sendResult: (data: unknown) => Promise<void | never>;
+    onDone: () => void;
+};
 
-const i = 2;
+interface TaskData {
+    matrix: number[][],
+    column: number[],
+}
 
-console.log("Imported from file: ", exampleImport, "\n", i);
+const matrixVectorProduct = (matrix: number[][], vector: number[]): number[] => {
+  let product = Array(matrix.length);
+
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    if (row.length != vector.length) {
+      throw `Mismatch in lengths of row ${i} of the matrix (length ${row.length}) and the vector (length ${vector.length})`;
+    }
+
+    let s = 0;
+
+    for (let j = 0; j < row.length; j++) {
+      s += row[j] * vector[j];
+    }
+    product[i] = s;
+  }
+
+  return product;
+}
+
+const run = async () => {
+  // The example core will help compute a large matrix-matrix product.
+  // In order to do this, it will receive a left-hand matrix (row-major), `matrix`, and a column of the right-hand matrix, `column`.
+  // The matrix-vector product of these will then be the column in the resulting matrix-matrix product at the position of `column`.
+  const { matrix, column } = await self.getData() as TaskData;
+
+  const product = matrixVectorProduct(matrix, column);
+
+  await self.sendResult(product);
+};
+
+run().then(() => self.onDone());
+
+export {};
