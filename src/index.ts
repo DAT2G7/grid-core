e5ac70995905ade3f258843d0121660aa6be5e54
Markus Dam <markus.dam123@gmail.com>
2023-04-23 02:43:00 +0200

    run prettier

--- src/index.ts
+++ src/index.ts
@@ -8,39 +8,42 @@ declare const self: DedicatedWorkerGlobalScope & {
 };
 
 interface TaskData {
+    matrix: number[][];
+    column: number[];
+}
+
+const matrixVectorProduct = (
     matrix: number[][],
-    column: number[],
-}
+    vector: number[]
+): number[] => {
+    let product = Array(matrix.length);
 
-const matrixVectorProduct = (matrix: number[][], vector: number[]): number[] => {
-  let product = Array(matrix.length);
+    for (let i = 0; i < matrix.length; i++) {
+        const row = matrix[i];
+        if (row.length != vector.length) {
+            throw `Mismatch in lengths of row ${i} of the matrix (length ${row.length}) and the vector (length ${vector.length})`;
+        }
 
-  for (let i = 0; i < matrix.length; i++) {
-    const row = matrix[i];
-    if (row.length != vector.length) {
-      throw `Mismatch in lengths of row ${i} of the matrix (length ${row.length}) and the vector (length ${vector.length})`;
+        let s = 0;
+
+        for (let j = 0; j < row.length; j++) {
+            s += row[j] * vector[j];
+        }
+        product[i] = s;
     }
 
-    let s = 0;
-
-    for (let j = 0; j < row.length; j++) {
-      s += row[j] * vector[j];
-    }
-    product[i] = s;
-  }
-
-  return product;
-}
+    return product;
+};
 
 const run = async () => {
-  // The example core will help compute a large matrix-matrix product.
-  // In order to do this, it will receive a left-hand matrix (row-major), `matrix`, and a column of the right-hand matrix, `column`.
-  // The matrix-vector product of these will then be the column in the resulting matrix-matrix product at the position of `column`.
-  const { matrix, column } = await self.getData() as TaskData;
+    // The example core will help compute a large matrix-matrix product.
+    // In order to do this, it will receive a left-hand matrix (row-major), `matrix`, and a column of the right-hand matrix, `column`.
+    // The matrix-vector product of these will then be the column in the resulting matrix-matrix product at the position of `column`.
+    const { matrix, column } = (await self.getData()) as TaskData;
 
-  const product = matrixVectorProduct(matrix, column);
+    const product = matrixVectorProduct(matrix, column);
 
-  await self.sendResult(product);
+    await self.sendResult(product);
 };
 
 run().then(() => self.onDone());
